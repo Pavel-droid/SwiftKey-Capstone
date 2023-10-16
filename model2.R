973aa08669b853d79add343d8324e70a840498f3
Prikryl Pavel (PS/NE-TX) <pra2bj@bosch.com>
2023-10-16 16:31:39 +0200

    pokus o predikci, z práce

--- model2.R
+++ model2.R
@@ -22,31 +22,29 @@ if(!dir.exists("final")){
 
 list <- unzip(download_zip, list = T)
 
-# corpus 99k
+# corpus 9.9k
 path_twitter <- list[11,1]
 path_news <- list[12,1]
 path_blogs <- list[13,1]
 
 con <- file(path_twitter, "r")
-tw1 <- readLines(con, 33000)
+tw1 <- readLines(con, 330)
 close(con)
 
 con <- file(path_news, "r")
-ne1 <- readLines(con, 33000)
+ne1 <- readLines(con, 330)
 close(con)
 
 con <- file(path_blogs, "r")
-bl1 <- readLines(con, 33000)
+bl1 <- readLines(con, 330)
 close(con)
 
 corpus_99 <- corpus(c(tw1, ne1, bl1))
 
-# tokens 99k
+# tokens 9.9k
 toks99 <- tokens(corpus_99, remove_punct = T, remove_numbers = T, remove_symbols = T,
                  remove_url = T)
-
 toks99_stem <- tokens_wordstem(toks99, language = "en")
-
 
 # n-grams
 gram2 <- tokens_ngrams(toks99_stem, n = 2)
@@ -57,9 +55,9 @@ dfm1 <- dfm(toks99_stem)
 dfm2 <- dfm(gram2)
 dfm3 <- dfm(gram3)
 
-dfm1 <- dfm_trim(dfm1, min_termfreq = 8, max_docfreq = 0.05, docfreq_type = "prop")
-dfm2 <- dfm_trim(dfm2, min_termfreq = 6, max_docfreq = 0.1, docfreq_type = "prop")
-dfm3 <- dfm_trim(dfm3, min_termfreq = 3, max_docfreq = 0.2, docfreq_type = "prop")
+dfm1 <- dfm_trim(dfm1, min_termfreq = 5, max_docfreq = 0.05, docfreq_type = "prop")
+dfm2 <- dfm_trim(dfm2, min_termfreq = 3, max_docfreq = 0.1, docfreq_type = "prop")
+dfm3 <- dfm_trim(dfm3, min_termfreq = 2, max_docfreq = 0.2, docfreq_type = "prop")
 
 # words
 sums1 <- colSums(dfm1)
@@ -82,5 +80,51 @@ words3 <- data.table(
 setkey(words1, word_1)
 setkey(words2, word_1, word_2)
 setkey(words3, word_1, word_2, word_3)
+
+# probabilities
+words1_prob <- words1[, prob := count / sum(words1$count), by = word_1]
+
+
+# trial
+raw_input <- "come in a few"
+
+inp <- tokens(raw_input, remove_punct = TRUE, remove_numbers = TRUE, remove_symbols = TRUE,
+                      remove_url = TRUE)
+inp <- tokens_wordstem(inp, language = "en")
+inp_words <- as.character(inp[[1]])
+inp_l <- length(inp_words)
+        
+if (inp_l >= 2) {
+        w_2 <- inp_words[inp_l - 1]
+        w_1 <- inp_words[inp_l]
+} 
+
+if (inp_l == 1) {
+        w_1 <- inp_words[inp_l]
+}        
+        
+if (inp_l == 0) {
+        print("Input must contain at least one word.")
+}        
+
+words3_f <- words3 |> filter(word_1 == w_2) |> filter(word_2 == w_1) |> arrange(-count)
+words2_f <- words2 |> filter(word_1 == w_1) |> arrange(-count)
+
+if (nrow(words3_f)>0) {
+        print(words3_f[1,3])
+                
+} else if (nrow(words2_f)>0){
+        print(words2_f[1,2])
+}
+
+
+
+
+
+
+
+
+
+
 
 
